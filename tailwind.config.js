7da495839ac475951dc1007927f7870b08616e21
Bilawal Hussain <117905020+bangash-0@users.noreply.github.com>
2023-10-03 00:52:54 +0500

    Main Body
    Done Some Changes in navbar for mobile version and started working on main body of project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
-  theme: {
-    extend: {
-        colors: {
-            'primary': '#22A5EF',
-            'secondary': {
-                100: '#F5F4F4',
-                200: '#fcfcfc',
-                300: '#6F6F6F'
-            }
-        },
-    },
-  },
-  plugins: [],
-}
+	content: ["./public/**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#22A5EF",
+				secondary: {
+					100: "#fcfcfc",
+					200: "#F5F4F4 ",
+					300: "#F6F6F6",
+					400: "#6F6F6F",
+				},
+
+				height: {
+					128: "32rem",
+				},
+
+				minHeight: {
+					128: "32rem",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
